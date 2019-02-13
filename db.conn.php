28b8c03a5d4d59fb0b61a8de3cc15b24ea9df46b
lauraayushi <royalsayushi@gmail.com>
2019-02-13 14:42:57 +0300

    Added changes

---
+++ db.conn.php
@@ -0,0 +1,10 @@
+<?php
+$servername = "localhost";
+$usernme = "root";
+$password = "";
+$dbname = "un_donation";
+$donorID = $_POST[];
+$surname = $_POST['surname']
+
+$conn = mysqli_connect($servername, $username
+                    $password, $dbname);
